194f847dc82810012bce1403ba4500d3b31c5bbc
A-SCharles <a.charles.edu@gmail.com>
2022-06-17 16:01:02 +0200

    updated

--- 03 ageToDays/script/script.js
+++ 03 ageToDays/script/script.js
@@ -3,36 +3,16 @@ const display = document.getElementById('days');
 const btnCalc = document.getElementById('calculate');
 btnCalc.addEventListener('click', (dob) => {
     dob = new Date(document.getElementById('DOB').value);
-
-    //calculate month difference from current date in time
-    var month_diff = Date.now() - dob.getTime();
-    console.log('Month ' + month_diff)
-    //convert the calculated difference in date format
-    var age_dt = new Date(month_diff);
-    console.log('agedt ' + age_dt)
-
-    //extract year from date
-    var year = age_dt.getFullYear();
-    console.log('year ' + year)
-
-    //now calculate the age of the user
-    var age = Math.abs(year - 1970);
-    console.log('age ' + age)
-
-    var days = age * 365;
-    console.log('days ' + days)
-
-    //display the calculated age
-    return (display.innerHTML =
-        "You are " + age + " years" + " and " + days + " days old");
+    let days = (new Date() - new Date(dob)) / (1000 * 60 * 60 * 24);
+    display.innerHTML = `${Math.round(days)} days from birth`;
 })
 
 const btnClear = document.getElementById('clear');
 btnClear.addEventListener('click', () => {
     document.getElementById('DOB').value = '';
     display.innerHTML = '';
+    document.getElementById('DOB').focus()
 })
-
 
 // copyright
 const Year = new Date().getFullYear();
